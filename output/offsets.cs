d5f1315ab8ec9243f646e7d3e6fcd4fd70b50426
swatzat-oss <swatzat@gmail.com>
2025-09-08 21:23:21 -0700

    update

---
+++ output/offsets.cs
@@ -0,0 +1,52 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-09-09 04:21:26.999054200 UTC
+
+namespace CS2Dumper.Offsets {
+    // Module: client.dll
+    public static class ClientDll {
+        public const nint dwCSGOInput = 0x1E3D470;
+        public const nint dwEntityList = 0x1D15F88;
+        public const nint dwGameEntitySystem = 0x1FB5EA8;
+        public const nint dwGameEntitySystem_highestEntityIndex = 0x20F0;
+        public const nint dwGameRules = 0x1E2F0F8;
+        public const nint dwGlobalVars = 0x1BE77F0;
+        public const nint dwGlowManager = 0x1E2F108;
+        public const nint dwLocalPlayerController = 0x1E1F1E8;
+        public const nint dwLocalPlayerPawn = 0x1BF2490;
+        public const nint dwPlantedC4 = 0x1E38160;
+        public const nint dwPrediction = 0x1BF23C0;
+        public const nint dwSensitivity = 0x1E2FC28;
+        public const nint dwSensitivity_sensitivity = 0x48;
+        public const nint dwViewAngles = 0x1E3DC20;
+        public const nint dwViewMatrix = 0x1E330F0;
+        public const nint dwViewRender = 0x1E33D80;
+        public const nint dwWeaponC4 = 0x1DD0838;
+    }
+    // Module: engine2.dll
+    public static class Engine2Dll {
+        public const nint dwBuildNumber = 0x5E2144;
+        public const nint dwNetworkGameClient = 0x5E1230;
+        public const nint dwNetworkGameClient_clientTickCount = 0x358;
+        public const nint dwNetworkGameClient_deltaTick = 0x23C;
+        public const nint dwNetworkGameClient_isBackgroundMap = 0x2C142F;
+        public const nint dwNetworkGameClient_localPlayer = 0xE8;
+        public const nint dwNetworkGameClient_maxClients = 0x230;
+        public const nint dwNetworkGameClient_serverTickCount = 0x23C;
+        public const nint dwNetworkGameClient_signOnState = 0x220;
+        public const nint dwWindowHeight = 0x8DA4D4;
+        public const nint dwWindowWidth = 0x8DA4D0;
+    }
+    // Module: inputsystem.dll
+    public static class InputsystemDll {
+        public const nint dwInputSystem = 0x45A20;
+    }
+    // Module: matchmaking.dll
+    public static class MatchmakingDll {
+        public const nint dwGameTypes = 0x1B5EE0;
+    }
+    // Module: soundsystem.dll
+    public static class SoundsystemDll {
+        public const nint dwSoundSystem = 0x3DEB70;
+        public const nint dwSoundSystem_engineViewData = 0x7C;
+    }
+}
