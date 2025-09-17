b63862e58369b85f5b4fc4a28a299c02a1c32b92
swatzat-oss <swatzat@gmail.com>
2025-09-17 16:21:13 -0700

    update

---
+++ output/buttons.hpp
@@ -0,0 +1,28 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-09-17 23:20:19.582472500 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    // Module: client.dll
+    namespace buttons {
+        constexpr std::ptrdiff_t attack = 0x1BD7280;
+        constexpr std::ptrdiff_t attack2 = 0x1BD7310;
+        constexpr std::ptrdiff_t back = 0x1BD7550;
+        constexpr std::ptrdiff_t duck = 0x1BD7820;
+        constexpr std::ptrdiff_t forward = 0x1BD74C0;
+        constexpr std::ptrdiff_t jump = 0x1BD7790;
+        constexpr std::ptrdiff_t left = 0x1BD75E0;
+        constexpr std::ptrdiff_t lookatweapon = 0x1E29180;
+        constexpr std::ptrdiff_t reload = 0x1BD71F0;
+        constexpr std::ptrdiff_t right = 0x1BD7670;
+        constexpr std::ptrdiff_t showscores = 0x1E29060;
+        constexpr std::ptrdiff_t sprint = 0x1BD7160;
+        constexpr std::ptrdiff_t turnleft = 0x1BD73A0;
+        constexpr std::ptrdiff_t turnright = 0x1BD7430;
+        constexpr std::ptrdiff_t use = 0x1BD7700;
+        constexpr std::ptrdiff_t zoom = 0x1E290F0;
+    }
+}
---
+++ output/offsets.hpp
@@ -0,0 +1,58 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-09-17 23:20:19.582472500 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace offsets {
+        // Module: client.dll
+        namespace client_dll {
+            constexpr std::ptrdiff_t dwCSGOInput = 0x1E29260;
+            constexpr std::ptrdiff_t dwEntityList = 0x1D02058;
+            constexpr std::ptrdiff_t dwGameEntitySystem = 0x1FA3118;
+            constexpr std::ptrdiff_t dwGameEntitySystem_highestEntityIndex = 0x20F0;
+            constexpr std::ptrdiff_t dwGameRules = 0x1E1E040;
+            constexpr std::ptrdiff_t dwGlobalVars = 0x1BD3130;
+            constexpr std::ptrdiff_t dwGlowManager = 0x1E1AE28;
+            constexpr std::ptrdiff_t dwLocalPlayerController = 0x1E0B118;
+            constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x1BDDE10;
+            constexpr std::ptrdiff_t dwPlantedC4 = 0x1E24380;
+            constexpr std::ptrdiff_t dwPrediction = 0x1BDDD40;
+            constexpr std::ptrdiff_t dwSensitivity = 0x1E1B938;
+            constexpr std::ptrdiff_t dwSensitivity_sensitivity = 0x48;
+            constexpr std::ptrdiff_t dwViewAngles = 0x1E29A10;
+            constexpr std::ptrdiff_t dwViewMatrix = 0x1E1EE00;
+            constexpr std::ptrdiff_t dwViewRender = 0x1E1FC20;
+            constexpr std::ptrdiff_t dwWeaponC4 = 0x1DBC4F0;
+        }
+        // Module: engine2.dll
+        namespace engine2_dll {
+            constexpr std::ptrdiff_t dwBuildNumber = 0x5EB3D4;
+            constexpr std::ptrdiff_t dwNetworkGameClient = 0x5EA4F0;
+            constexpr std::ptrdiff_t dwNetworkGameClient_clientTickCount = 0x358;
+            constexpr std::ptrdiff_t dwNetworkGameClient_deltaTick = 0x23C;
+            constexpr std::ptrdiff_t dwNetworkGameClient_isBackgroundMap = 0x2C142F;
+            constexpr std::ptrdiff_t dwNetworkGameClient_localPlayer = 0xE8;
+            constexpr std::ptrdiff_t dwNetworkGameClient_maxClients = 0x230;
+            constexpr std::ptrdiff_t dwNetworkGameClient_serverTickCount = 0x23C;
+            constexpr std::ptrdiff_t dwNetworkGameClient_signOnState = 0x220;
+            constexpr std::ptrdiff_t dwWindowHeight = 0x8E967C;
+            constexpr std::ptrdiff_t dwWindowWidth = 0x8E9678;
+        }
+        // Module: inputsystem.dll
+        namespace inputsystem_dll {
+            constexpr std::ptrdiff_t dwInputSystem = 0x45A20;
+        }
+        // Module: matchmaking.dll
+        namespace matchmaking_dll {
+            constexpr std::ptrdiff_t dwGameTypes = 0x1B5EE0;
+        }
+        // Module: soundsystem.dll
+        namespace soundsystem_dll {
+            constexpr std::ptrdiff_t dwSoundSystem = 0x3E3E30;
+            constexpr std::ptrdiff_t dwSoundSystem_engineViewData = 0x7C;
+        }
+    }
+}
