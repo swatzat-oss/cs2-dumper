7d99d9d2f738f9c88401ae107d0a6cf7e8dea4c1
swatzat-oss <swatzat@gmail.com>
2025-09-16 23:03:37 -0700

    update

---
+++ output/buttons.hpp
@@ -0,0 +1,28 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-09-17 05:57:23.015495800 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    // Module: client.dll
+    namespace buttons {
+        constexpr std::ptrdiff_t attack = 0x1BD7180;
+        constexpr std::ptrdiff_t attack2 = 0x1BD7210;
+        constexpr std::ptrdiff_t back = 0x1BD7450;
+        constexpr std::ptrdiff_t duck = 0x1BD7720;
+        constexpr std::ptrdiff_t forward = 0x1BD73C0;
+        constexpr std::ptrdiff_t jump = 0x1BD7690;
+        constexpr std::ptrdiff_t left = 0x1BD74E0;
+        constexpr std::ptrdiff_t lookatweapon = 0x1E29000;
+        constexpr std::ptrdiff_t reload = 0x1BD70F0;
+        constexpr std::ptrdiff_t right = 0x1BD7570;
+        constexpr std::ptrdiff_t showscores = 0x1E28EE0;
+        constexpr std::ptrdiff_t sprint = 0x1BD7060;
+        constexpr std::ptrdiff_t turnleft = 0x1BD72A0;
+        constexpr std::ptrdiff_t turnright = 0x1BD7330;
+        constexpr std::ptrdiff_t use = 0x1BD7600;
+        constexpr std::ptrdiff_t zoom = 0x1E28F70;
+    }
+}
