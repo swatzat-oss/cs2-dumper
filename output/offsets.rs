72a4aebf04c20670ca21f6562afcc60465f8087c
swatzat-oss <swatzat@gmail.com>
2025-10-01 16:51:17 -0700

    update

---
+++ output/offsets.rs
@@ -0,0 +1,56 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-10-01 23:49:46.742300100 UTC
+
+#![allow(non_upper_case_globals, unused)]
+
+pub mod cs2_dumper {
+    pub mod offsets {
+        // Module: client.dll
+        pub mod client_dll {
+            pub const dwCSGOInput: usize = 0x1E37B30;
+            pub const dwEntityList: usize = 0x1D10658;
+            pub const dwGameEntitySystem: usize = 0x1FB1848;
+            pub const dwGameEntitySystem_highestEntityIndex: usize = 0x20F0;
+            pub const dwGameRules: usize = 0x1E2C978;
+            pub const dwGlobalVars: usize = 0x1BE1120;
+            pub const dwGlowManager: usize = 0x1E29818;
+            pub const dwLocalPlayerController: usize = 0x1E19958;
+            pub const dwLocalPlayerPawn: usize = 0x1BEBEA0;
+            pub const dwPlantedC4: usize = 0x1E32098;
+            pub const dwPrediction: usize = 0x1BEBDD0;
+            pub const dwSensitivity: usize = 0x1E2A278;
+            pub const dwSensitivity_sensitivity: usize = 0x48;
+            pub const dwViewAngles: usize = 0x1E381E0;
+            pub const dwViewMatrix: usize = 0x1E2D6D0;
+            pub const dwViewRender: usize = 0x1E2E4F0;
+            pub const dwWeaponC4: usize = 0x1DCADE8;
+        }
+        // Module: engine2.dll
+        pub mod engine2_dll {
+            pub const dwBuildNumber: usize = 0x5EB3D4;
+            pub const dwNetworkGameClient: usize = 0x5EA4F0;
+            pub const dwNetworkGameClient_clientTickCount: usize = 0x358;
+            pub const dwNetworkGameClient_deltaTick: usize = 0x23C;
+            pub const dwNetworkGameClient_isBackgroundMap: usize = 0x2C142F;
+            pub const dwNetworkGameClient_localPlayer: usize = 0xE8;
+            pub const dwNetworkGameClient_maxClients: usize = 0x230;
+            pub const dwNetworkGameClient_serverTickCount: usize = 0x23C;
+            pub const dwNetworkGameClient_signOnState: usize = 0x220;
+            pub const dwWindowHeight: usize = 0x8E9664;
+            pub const dwWindowWidth: usize = 0x8E9660;
+        }
+        // Module: inputsystem.dll
+        pub mod inputsystem_dll {
+            pub const dwInputSystem: usize = 0x45A20;
+        }
+        // Module: matchmaking.dll
+        pub mod matchmaking_dll {
+            pub const dwGameTypes: usize = 0x1B5EE0;
+        }
+        // Module: soundsystem.dll
+        pub mod soundsystem_dll {
+            pub const dwSoundSystem: usize = 0x3E3E30;
+            pub const dwSoundSystem_engineViewData: usize = 0x7C;
+        }
+    }
+}
