6fd570f35b62aceec2cab7516bf823ac6240b21b
fentywenty <jpettett28@gmail.com>
2026-03-05 21:52:37 -0500

    Add files via upload

---
+++ output/buttons.hpp
@@ -0,0 +1,28 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-03-06 02:47:12.867942800 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    // Module: client.dll
+    namespace buttons {
+        constexpr std::ptrdiff_t attack = 0x205F8F0;
+        constexpr std::ptrdiff_t attack2 = 0x205F980;
+        constexpr std::ptrdiff_t back = 0x205FBC0;
+        constexpr std::ptrdiff_t duck = 0x205FE90;
+        constexpr std::ptrdiff_t forward = 0x205FB30;
+        constexpr std::ptrdiff_t jump = 0x205FE00;
+        constexpr std::ptrdiff_t left = 0x205FC50;
+        constexpr std::ptrdiff_t lookatweapon = 0x2316F60;
+        constexpr std::ptrdiff_t reload = 0x205F860;
+        constexpr std::ptrdiff_t right = 0x205FCE0;
+        constexpr std::ptrdiff_t showscores = 0x2316E40;
+        constexpr std::ptrdiff_t sprint = 0x205F7D0;
+        constexpr std::ptrdiff_t turnleft = 0x205FA10;
+        constexpr std::ptrdiff_t turnright = 0x205FAA0;
+        constexpr std::ptrdiff_t use = 0x205FD70;
+        constexpr std::ptrdiff_t zoom = 0x2316ED0;
+    }
+}
