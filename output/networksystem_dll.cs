808a2999788beb4aa3f360bee60b72f3b9ef7d56
fentywenty <jpettett28@gmail.com>
2026-03-18 18:47:13 -0400

    Add files via upload

---
+++ output/networksystem_dll.cs
@@ -0,0 +1,18 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-03-18 22:44:16.892512500 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: networksystem.dll
+    // Class count: 0
+    // Enum count: 1
+    public static class NetworksystemDll {
+        // Alignment: 4
+        // Member count: 4
+        public enum OutOfPVSUpdates_t : uint {
+            OOPVSUpdates_OptOut = 0x0,
+            OOPVSUpdates_OptIn = 0x1,
+            OOPVSUpdates_Default = 0x2,
+            OOPVSUpdates_Count = 0x3
+        }
+    }
+}
